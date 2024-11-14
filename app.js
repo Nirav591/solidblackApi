42b864d80c5822ab3dfe3fac098be7af16ddb04a
Nirav591 <nirav.v.lathiya@gmail.com>
2024-11-14 13:54:49 +0000

    added

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+// Set the port
+const PORT = 3000;
+
+// Define a route for the "Hello World" response
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
